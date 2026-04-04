256415cb7b2dc5c6cd256e3759bd386ebe9586b9
Victor Johnson <okikevictorodinaka@gmail.com>
2026-04-04 10:58:54 -0700

    trying the cors again

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,8 +36,6 @@ app.use(
 );
 
 app.use(express.json());
-app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:8081/",
@@ -51,6 +49,7 @@ app.use(
     credentials: true,
   }),
 );
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
