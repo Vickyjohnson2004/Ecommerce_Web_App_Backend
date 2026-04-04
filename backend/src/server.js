1e72b5d2f140d17d22f3826b144405476c9bc8e3
Victor Johnson <okikevictorodinaka@gmail.com>
2026-04-04 11:02:22 -0700

    allowed cors before clerk auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -36,8 +36,7 @@ app.use(
 );
 
 app.use(express.json());
-app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
+
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:8081/",
@@ -51,6 +50,8 @@ app.use(
     credentials: true,
   }),
 );
+app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+// app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
